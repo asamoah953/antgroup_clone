8182490ae6fc1bfaa1b3949797e053dc993cb3ec
asamoah953 <asamoahclinton953@gmail.com>
2025-05-26 20:29:24 +0100

    Added more  components

--- ant-home_page/src/components/header.jsx
+++ ant-home_page/src/components/header.jsx
@@ -1,8 +1,12 @@
 import HeaderImage from '../assets/antgroup.png';
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+
 
 
 function Header() {
+    const [languages, setLanguages] = useState(false);
+
     return(
         <>
         <div className='header_bar'>
@@ -13,8 +17,22 @@ function Header() {
             <Link to='#' className='all_links'><h3 className='esg'>ESG</h3></Link>
             <Link to='#' className='all_links'><h3 className='news'>News & Media</h3></Link>
             <Link to='#' className='all_links'><h3 className='join'>Join Us</h3></Link>
-            <Link to='#' className='all_links'><h3 className='en'>EN <span>&#9660;</span></h3></Link>
+            <div
+  className="language-wrapper"
+  onClick={()=>setLanguages(true)}
+  // onMouseEnter={() => setLanguages(true)}
+  // onMouseLeave={() => setLanguages(false)}
+>
+  <Link to="#" className="all_links">
+    <h3 className="en">EN <span>&#9660;</span></h3>
+  </Link>
 
+  {languages && (
+    <div className="languages_container">
+      <h3 className="languages">languages are here</h3>
+    </div>
+  )}
+</div>
         </div>
         </>
     )
